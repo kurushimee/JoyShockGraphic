47d012f027ceca45a934c6746d23ad5e6d63cc43
Ivan Ermakov <jermacoff@gmail.com>
2022-11-10 19:18:22 +0700

    Add initial configurator init

--- joyshockgraphic/dmanager.py
+++ joyshockgraphic/dmanager.py
@@ -8,11 +8,6 @@ class DManager:
         self.cur = self.con.cursor()
 
     def create_profile(self, display_name: str, file_name: str):
-        # Create a new profile entry in the profiles table
-        self.insert(
-            "profiles",
-            (display_name, file_name),
-        )
         # Create a new table for the profile
         self.cur.execute(
             f"""CREATE TABLE "{file_name}" (
@@ -23,53 +18,74 @@ class DManager:
                                 event    STRING
                              );"""
         )
-        self.con.commit()
+        # Create a new profile entry in the profiles table
+        self.insert(
+            "profiles",
+            (display_name, file_name),
+        )
 
-    # Change the display name in the profiles list
-    # and file name of a profile in the database
-    def edit_profile(self, old_data: tuple, new_data: tuple):
+    def edit_profile(self, name_old: tuple, name_new: tuple):
+        # Rename profile's table
+        self.cur.execute(
+            f'ALTER TABLE "{name_old[1]}" RENAME TO "{name_new[1]}"'
+        )
+        # Change the display name
         self.update(
             "profiles",
             "display_name",
-            new_data[0],
-            f'display_name="{old_data[0]}"',
+            name_new[0],
+            f'display_name="{name_old[0]}"',
         )
+        # Change the file name
         self.update(
             "profiles",
             "file_name",
-            new_data[1],
-            f'file_name="{old_data[1]}"',
+            name_new[1],
+            f'file_name="{name_old[1]}"',
         )
 
-    # Select entries from the database
+    def delete_profile(self, display_name: str):
+        # Find file name of the profile based on display name
+        file_name = self.select(
+            "file_name", "profiles", f'display_name = "{display_name}"'
+        )[0][0]
+        # Delete profile's table
+        self.cur.execute(f'DROP TABLE "{file_name}"')
+        # Delete profile's entry in the database
+        self.delete(
+            "profiles",
+            f'display_name = "{display_name}" AND file_name = "{file_name}"',
+        )
+
     def select(self, field: str, table: str, condition=""):
+        # Select entries from the database
         if condition != "":
             condition = " WHERE " + condition
         return self.cur.execute(
-            f"SELECT {field} FROM {table}{condition}"
+            f'SELECT {field} FROM "{table}"{condition}'
         ).fetchall()
 
-    # Insert new entry to the database
     def insert(self, table: str, values: tuple, fields=()):
+        # Insert new entry to the database
         f = "" if fields == () else ",".join([f'"{x}"' for x in fields])
         v = ",".join([f'"{x}"' for x in values])
-        self.cur.execute(f"INSERT INTO {table}{f} VALUES({v})")
+        self.cur.execute(f'INSERT INTO "{table}"{f} VALUES({v})')
         self.con.commit()
 
-    # Update an entry in the database
     def update(self, table: str, field: str, value: str, condition=""):
+        # Update an entry in the database
         if condition != "":
             condition = "WHERE " + condition
         self.cur.execute(
-            f"""UPDATE {table}
+            f"""UPDATE "{table}"
                 SET {field} = "{value}"
                 {condition}"""
         )
         self.con.commit()
 
-    # Delete an entry in the database
     def delete(self, table, condition):
-        self.cur.execute(f"DELETE FROM {table} WHERE {condition}")
+        # Delete an entry in the database
+        self.cur.execute(f'DELETE FROM "{table}" WHERE {condition}')
         self.con.commit()
 
     def close(self):
--- joyshockgraphic/joyshockgraphic.py
+++ joyshockgraphic/joyshockgraphic.py
@@ -22,6 +22,7 @@ class MainWindow(QMainWindow):
 
         # Connect button groups
         self.bgLibrary.buttonClicked.connect(self.handle_profile)
+        self.bgConfig.buttonClicked.connect(self.config)
         self.bgLoadInput.buttonClicked.connect(self.load_input)
         self.bgPickBind.buttonClicked.connect(self.pick_bind)
         self.bgSwitchInput.buttonClicked.connect(self.switch_input)
@@ -31,17 +32,20 @@ class MainWindow(QMainWindow):
         for profile in self.dman.select("display_name", "profiles"):
             display_name = profile[0]
             self.lwProfiles.addItem(display_name)
+        self.selection_changed()
 
     def selection_changed(self):
-        if self.lwProfiles.currentItem().text() in [
+        currentItem = self.lwProfiles.currentItem()
+        if currentItem and currentItem.text() in [
             x[0] for x in self.dman.select("display_name", "profiles")
         ]:
             condition = True
+            self.configure()
         else:
             condition = False
+            self.mainTabs.setTabEnabled(1, False)
         self.pbEdit.setEnabled(condition)
         self.pbDelete.setEnabled(condition)
-        self.pbConfigure.setEnabled(condition)
 
     def handle_profile(self, sender: QPushButton):
         cond = sender.objectName()
@@ -49,10 +53,11 @@ class MainWindow(QMainWindow):
             self.create()
         elif cond == "pbEdit":
             self.edit()
-        elif cond == "pbConfigure":
-            self.configure()
         elif cond == "pbDelete":
             self.delete()
+
+    def config(self, sender: QPushButton):
+        pass
 
     def load_input(self, sender: QPushButton):
         pass
@@ -64,7 +69,15 @@ class MainWindow(QMainWindow):
             pass
 
     def switch_input(self, sender: QPushButton):
-        pass
+        tabs = {
+            "pbButtons": 0,
+            "pbDpad": 1,
+            "pbTriggers": 2,
+            "pbJoysticks": 3,
+            "pbTouchpad": 4,
+            "pbGyro": 5,
+        }
+        self.inputWidgets.setCurrentIndex(tabs[sender.objectName()])
 
     def create(self):
         # Open profile creation dialog
@@ -91,20 +104,42 @@ class MainWindow(QMainWindow):
                         "file_name",
                         "profiles",
                         f'display_name = "{display_name}"',
-                    ),
+                    )[0][0],
                 ),
                 (dlg.leDisplayName.text(), dlg.leFileName.text()),
             )
             self.populate_list()
 
     def configure(self):
-        pass
+        # Enable configurator and set current profile
+        self.mainTabs.setTabEnabled(1, True)
+        self.profile = self.lwProfiles.currentItem().text()
+        self.init_configurator()
 
     def delete(self):
-        pass
+        # Deletes selected profile
+        self.dman.delete_profile(self.lwProfiles.currentItem().text())
+        self.populate_list()
 
     def closeEvent(self, event):
         self.dman.close()
+
+    def init_configurator(self):
+        for button in self.bgPickBind.buttons():
+            i = (
+                0
+                if button.objectName()[-2] != r"^_[1-9]$"
+                else int(button.objectName()[-1])
+            )
+            print(i)
+            special = {"pbMinus": "-", "pbPlus": "+"}
+            name = button.objectName()[:-2] if i > 0 else button.objectName()
+            name = name[2:] if name not in special else special[name]
+            button.setText(
+                self.dman.select("bind", self.profile, f'command = "{name}"')[
+                    i
+                ]
+            )
 
 
 if __name__ == "__main__":
