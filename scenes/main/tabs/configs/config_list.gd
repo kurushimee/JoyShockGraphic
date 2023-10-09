a9198dbd4f5f07d1c98cd4f2dd87d1528df5e987
iver0 <ermakov@skiff.com>
2023-10-09 17:03:27 +0700

    Add initial logic for config handling

--- scenes/main/tabs/configs/config_list.gd
+++ scenes/main/tabs/configs/config_list.gd
@@ -1,5 +1,34 @@
 extends ItemList
 
+const SEARCH_DIR := "/AutoLoad"
+var jsm_path: String
+
+var configs := []
+
 
 func _ready() -> void:
-	pass
+	jsm_path = OS.get_executable_path().get_base_dir()
+	search_configs(jsm_path + SEARCH_DIR)
+	init_list()
+
+
+func search_configs(path):
+	var dir := DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+
+		var file_name := dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir():
+				var config := file_name
+				print("Found config: " + config)
+				configs.append(config)
+
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+
+
+func init_list():
+	for config in configs:
+		print(config.get_basename())
